7ca1ebda237a1305f3a190564718292754ca93b6
yanua47 <53637171+yanua47@users.noreply.github.com>
2019-08-03 14:06:59 +0700

    Add files via upload

--- safelink.js
+++ safelink.js
@@ -18,7 +18,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://www.blogger.com/u/1/blogger.g?blogID=9104696993691804207#editor/target=post;postID=439158823658778827;onPublishedMenu=allposts;onClosedMenu=allposts;postNum=0;src=postname?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://omaeanime.com/one-piece-sub-indo/?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
